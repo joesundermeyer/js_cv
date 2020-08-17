95e73cde7f0c53e3fe4e53921f13f3eb78d7672d
joesundermeyer <joe.sundermeyer@gmail.com>
2020-08-16 21:50:10 -0400

    generated deployables

--- src/app/components/skils/skils.component.ts
+++ src/app/components/skils/skils.component.ts
@@ -9,25 +9,31 @@ import { Component, OnInit } from '@angular/core';
         </header> 
             <p>Digital Accessibility/Equity</p>
             <div class="container-3">
-              <div class="skills dae">90%</div>
+              <div class="skills dae skills-color">90%</div>
             </div>
             <h2 class="h2-exp">Advanced</h2>
             <p class="p-spaced">I have been working on accessibility for the past 8 years or so, most notably in the past 3, during my time at a startup focused employment for people with disabilities and, during the past year, as a digital accessibility expert for government of Johnson County, Kansas.</p>
 
-            <p>CSS</p>
+            <p>Copywriting</p>
             <div class="container-3">
-              <div class="skills css">80%</div>
+              <div class="skills copy skills-copy">95%</div>
             </div>
+            <h2 class="h2-exp">Expert</h2>
+            <p class="p-spaced">My first job after college was as a journalist. Writing and editing have been part of every job I've had since. I excel at Marketing and Advertising copywriting and writing for the web.</p>
 
-            <p>JavaScript</p>
+            <p>Angular CLI, Node.js, GitHub</p>
             <div class="container-3">
-              <div class="skills js">65%</div>
+              <div class="skills cli skills-cli">75%</div>
             </div>
+            <h2 class="h2-exp">Capable</h2>
+            <p class="p-spaced">My first job after college was as a journalist. Writing and editing have been part of every job I've had since. I excel at Marketing and Advertising copywriting and writing for the web.</p>
 
-            <p>PHP</p>
+            <p>HTML/CSS</p>
             <div class="container-3">
-              <div class="skills php">60%</div>
+            <div class="skills css skills-css">80%</div>
             </div>
+            <h2 class="h2-exp">Advanced</h2>
+            <p class="p-spaced">My first job after college was as a journalist. Writing and editing have been part of every job I've had since. I excel at Marketing and Advertising copywriting and writing for the web.</p>
   `,
   styles: [
   ]
